18faf66aa4337dc4f53676470df3e755964e6917
Efkan S. Goktepe <76130958+serhatgktp@users.noreply.github.com>
2023-01-09 17:57:36 -0500

    Added dev folders

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='containers'>
+        <div className='firstContainer'>
+          <h1 className="apple-garamond">A Computer Scientist currently studying at the University of Toronto.</h1>
+        </div>
+        <div className='secondContainer sf-pro'>
+          <p>Hey! My name is Efkan and I'm in my 4th year of Computer Science.</p>
+          <p>I have 8 months of full-time, and over 12 months of part-time/contract work experience as a Software Developer.</p>
+          <p>My contributions are mostly in backend development, infrastructure, and data warehousing, but I play across the entire tech stack!</p>
+          <p>I am currently a part-time developer at Alphabyte Solutions, developing and managing data pipelines for clients.</p>
+          <p>Aside from work and studies, I am an Academic Representative of AMACSS, and the President of the Game Dev Club at University of Toronto Scarborough.</p>
+          <p>I love meeting new people and exploring opportunities - feel free to reach out!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
