62797f36ff3c300f22a746faae7f50f8b0ad54cf
Efkan S. Goktepe <76130958+serhatgktp@users.noreply.github.com>
2023-01-12 01:19:57 -0500

    Updated source folder to match build folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import './App.css';
 
+import github_logo from './assets/github.svg'
+import linkedin_logo from './assets/linkedin.png'
+import resume_logo from './assets/resume.svg'
+import resume_pdf from './assets/resume.pdf'
+
 function App() {
   return (
     <div className="App">
-      <div className='containers'>
-        <div className='firstContainer'>
-          <h1 className="apple-garamond">A Computer Scientist currently studying at the University of Toronto.</h1>
+      <div className="containers">
+        <div className="firstContainer">
+          <h1 className="apple-garamond">I'm a software developer, with a sense of humor.<br/>Welcome!</h1>
         </div>
-        <div className='secondContainer sf-pro'>
-          <p>Hey! My name is Efkan and I'm in my 4th year of Computer Science.</p>
-          <p>I have 8 months of full-time, and over 12 months of part-time/contract work experience as a Software Developer.</p>
-          <p>My contributions are mostly in backend development, infrastructure, and data warehousing, but I play across the entire tech stack!</p>
-          <p>I am currently a part-time developer at Alphabyte Solutions, developing and managing data pipelines for clients.</p>
-          <p>Aside from work and studies, I am an Academic Representative of AMACSS, and the President of the Game Dev Club at University of Toronto Scarborough.</p>
+        <div className="secondContainer sf-pro">
+          <p>👨 My name is Efkan [Serhat] Goktepe</p>
+
+          <p>🎓 4th year of Computer Science at <a href="https://www.utoronto.ca/">University of Toronto</a></p>
+
+          <p>🔧 Currently a Software Developer at <a href="https://alphabytesolutions.com/">Alphabyte Solutions.</a></p>
+
+          <p>🗺️ Extracurriculars:<br/>
+            <ul>
+                <li>🕹️ President, <a href="https://csec.club/gamers/">UTSC Game Dev Club</a></li>
+                <li>✏️ Academic Representative, <a href="https://amacss.org/">AMACSS</a></li>
+            </ul>
+          </p>
+
+          <p>My contributions are mostly in backend development and infrastructure, but I play across the entire tech stack!</p>
+
           <p>I love meeting new people and exploring opportunities - feel free to reach out!</p>
+
+          <div className="media-icons">
+            <a href="https://www.linkedin.com/in/serhatgoktepe">
+              <img alt="linkedin-icon" src={linkedin_logo}/>
+            </a>
+
+            <a href="https://github.com/serhatgktp">
+              <img alt="github-icon" src={github_logo} target=""/>
+            </a>
+
+            <a href={resume_pdf}>
+              <img alt="resume-icon" src={resume_logo} target=""/>
+            </a>
+          </div>
         </div>
       </div>
     </div>
