d473008b4151857bad654a3890e3f6390c9d90db
Efkan S. Goktepe <76130958+serhatgktp@users.noreply.github.com>
2023-01-15 16:09:58 -0500

    fixed routing issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 
 import Introduction from './components/Introduction';
+import WIP from './components/WIP';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/personal-website/' exact element={<Introduction />}/>
-      </Routes>
-    </Router>
+    <div id='root'>
+      {/* <Introduction /> */}
+      <Router>
+        <Routes>
+          <Route path='/personal-website/' exact element={<Introduction />} />
+          <Route path='/personal-website/wip/' exact element={<WIP />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
---
+++ src/components/WIP.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WIP = () => {
+  return (
+    <div className="wip-header" style={{width:'100%', paddingTop:"5em"}}>
+        <h1 className="apple-garamond" style={{textAlign: "center"}}>This page is a work-in-progress!</h1>
+    </div>
+  )
+}
+
+export default WIP
